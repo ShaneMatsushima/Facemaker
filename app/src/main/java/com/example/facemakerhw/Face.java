d02411a0be60e2bf61d3db62d08da484aaa59f26
ShaneMatsushima <skm1901@iolani.org>
2020-09-09 13:46:57 -1000

    randomize function update in face class

--- app/src/main/java/com/example/facemakerhw/Face.java
+++ app/src/main/java/com/example/facemakerhw/Face.java
@@ -1,5 +1,7 @@
 package com.example.facemakerhw;
 
+import java.util.Random;
+
 public class Face {
     private int skinColor;
     private int eyeColor;
@@ -11,6 +13,21 @@ public class Face {
     }
 
     private void randomize() {
+        /**
+         * External Citation
+         * Date: 9/9/2020
+         * Problem: Did not know how to randomize numbers
+         * Resource: https://www.educative.io/edpresso/how-to-generate-random-numbers-in-java
+         * Solution: I used the sample code to guide the construction of generating random numbers
+         */
+        Random rand = new Random();
+        int upperBound = 255;
+
+        //generates values from 0 - 254
+        skinColor = rand.nextInt(upperBound);
+        eyeColor = rand.nextInt(upperBound);
+        hairColor = rand.nextInt(upperBound);
+        hairStyle = rand.nextInt(4); //not a color, only has 3 choices
 
     }
 
