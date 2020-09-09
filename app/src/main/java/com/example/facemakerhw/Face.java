2ee859cc9c57ce58cd069b2df380e57fea782b75
ShaneMatsushima <skm1901@iolani.org>
2020-09-09 13:41:36 -1000

    Gui update and Face class created

---
+++ app/src/main/java/com/example/facemakerhw/Face.java
@@ -0,0 +1,17 @@
+package com.example.facemakerhw;
+
+public class Face {
+    private int skinColor;
+    private int eyeColor;
+    private int hairColor;
+    private int hairStyle;
+
+    public Face(){
+        randomize();
+    }
+
+    private void randomize() {
+
+    }
+
+}
--- app/src/main/java/com/example/facemakerhw/MainActivity.java
+++ app/src/main/java/com/example/facemakerhw/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.facemakerhw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * External Citation
+         * Date: 9/9/2020
+         * Problem: Did not know how to add items into a spinner display
+         * Resource: https://stackoverflow.com/questions/5241660/how-to-add-items-to-a-spinner-in-android
+         * Solution: I used the sample code to guide the construction and addition of items into a spinner
+         */
+        Spinner hairStyleSpinner = (Spinner) findViewById(R.id.spinnerHairChoice);
+        String[]  hairStyle = new String[] {"Curly", "Straight", "Wild"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, hairStyle);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hairStyleSpinner.setAdapter(adapter);
+
+
     }
 }
